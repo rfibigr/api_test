efafd77771de75c408c48bcf8d0f27e0d9f9abd8
rfibigr <rfibigr@student.42.fr>
2019-04-18 20:02:30 +0200

    unit test to complete

--- src/test/java/com/test/test_soletanche/service/FilesServiceTest.java
+++ src/test/java/com/test/test_soletanche/service/FilesServiceTest.java
@@ -1,35 +1,30 @@
 package com.test.test_soletanche.service;
 
-import com.test.test_soletanche.DAO.FileDao;
-import com.test.test_soletanche.model.FilesDetail;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
 
-import static org.junit.Assert.*;
-
-public class FilesServiceTest {
-
-    FilesService filesService;
-
-    @Test
-    public void getAllFiles() {
-        List<FilesDetail> fileList = filesService.getAllFiles();
-        assertEquals(5, fileList.size());
-        assertEquals("Empty", fileList.get(0).getFileName());
-        assertEquals("image", fileList.get(2).getFileName());
-    }
-
-    @Test
-    public void getFileById() {
-        FilesDetail filesDetail0 = filesService.getFileById(0);
-        assertEquals("Empty", filesDetail0.getFileName());
-        FilesDetail filesDetail2 = filesService.getFileById(0);
-        assertEquals("image", filesDetail2.getFileName());
-    }
-
-    @Test
-    public void getFileByType() {
-    }
-}
+//@SpringBootTest
+//
+//    @Test
+//    public void getAllFiles() {
+//        System.out.println(filesService);
+//        List<FilesDetail> fileList = filesService.getAllFiles();
+//        assertEquals(5, fileList.size());
+//        assertEquals("Empty", fileList.get(0).getFileName());
+//        assertEquals("image", fileList.get(2).getFileName());
+//    }
+//
+//    @Test
+//    public void getFileById() {
+//        FilesService filesService = new FilesService();
+//        FilesDetail filesDetail0 = filesService.getFileById(0);
+//        assertEquals("Empty", filesDetail0.getFileName());
+//        FilesDetail filesDetail2 = filesService.getFileById(0);
+//        assertEquals("image", filesDetail2.getFileName());
+//    }
+//
+//    @Test
+//    public void getFileByType() {
+//    }
+//}
