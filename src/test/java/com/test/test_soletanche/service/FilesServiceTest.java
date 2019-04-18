71c139d077b3127911266ac611999cfc03ed5f2e
rfibigr <rfibigr@student.42.fr>
2019-04-18 20:02:50 +0200

    unit test to complete

--- src/test/java/com/test/test_soletanche/service/FilesServiceTest.java
+++ src/test/java/com/test/test_soletanche/service/FilesServiceTest.java
@@ -4,27 +4,29 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
-//@SpringBootTest
-//
-//    @Test
-//    public void getAllFiles() {
-//        System.out.println(filesService);
-//        List<FilesDetail> fileList = filesService.getAllFiles();
-//        assertEquals(5, fileList.size());
-//        assertEquals("Empty", fileList.get(0).getFileName());
-//        assertEquals("image", fileList.get(2).getFileName());
-//    }
-//
-//    @Test
-//    public void getFileById() {
-//        FilesService filesService = new FilesService();
-//        FilesDetail filesDetail0 = filesService.getFileById(0);
-//        assertEquals("Empty", filesDetail0.getFileName());
-//        FilesDetail filesDetail2 = filesService.getFileById(0);
-//        assertEquals("image", filesDetail2.getFileName());
-//    }
-//
-//    @Test
-//    public void getFileByType() {
-//    }
-//}
+/*
+@SpringBootTest
+
+@Test
+public void getAllFiles() {
+System.out.println(filesService);
+List<FilesDetail> fileList = filesService.getAllFiles();
+assertEquals(5, fileList.size());
+assertEquals("Empty", fileList.get(0).getFileName());
+assertEquals("image", fileList.get(2).getFileName());
+}
+
+@Test
+public void getFileById() {
+FilesService filesService = new FilesService();
+FilesDetail filesDetail0 = filesService.getFileById(0);
+assertEquals("Empty", filesDetail0.getFileName());
+FilesDetail filesDetail2 = filesService.getFileById(0);
+assertEquals("image", filesDetail2.getFileName());
+}
+
+@Test
+public void getFileByType() {
+}
+}
+*/
